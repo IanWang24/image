66bfb890ba30790e7129652bbae1bd5be0307e2b
IanWang24 <50164176+IanWang24@users.noreply.github.com>
2021-05-07 01:35:56 +0800

    Add files via upload

--- image_upload/imageupload/admin.py
+++ image_upload/imageupload/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from imageupload.models import UploadImage
+from imageupload.models import UploadImage,result_img
 
 
 admin.site.register(UploadImage)
+admin.site.register(result_img)
